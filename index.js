3e2a24c8bb0d6890b7de87dfcca092b9b39f7031
veronicacheung1105 <veronica.cheung1105@gmail.com>
2018-07-16 21:12:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file.
+function lowerCase(name){
+  return name.toLowerCase();
+}
+
+function lowerCaseStudentNames(array){
+  return array.map(lowerCase);
+}
+
+function name(nameo){
+  return ""
+}
+
+function nameToAttributes(arrayz){
+  return arrayz.map(name)
+}
